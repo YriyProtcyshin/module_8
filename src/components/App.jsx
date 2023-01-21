7d1920e714964c1f08eac2781163b5ede247d103
YriyProtcyshin <yriy.protcyshin@gmail.com>
2023-01-21 18:08:06 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import { Layout } from './Layout';
 import { Home } from 'page/Home';
+import { RegisterForm } from './Register/Register';
 export const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="/register" />
+          <Route path="register" element={<RegisterForm />} />
         </Route>
       </Routes>
     </div>
