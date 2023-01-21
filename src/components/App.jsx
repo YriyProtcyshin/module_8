15e093671deeaf942ca91e470bbdc4b630956e1a
YriyProtcyshin <yriy.protcyshin@gmail.com>
2023-01-21 17:09:54 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import { Layout } from './Layout';
+import { Home } from 'page/Home';
 export const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Layout />}></Route>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/register" />
+        </Route>
       </Routes>
     </div>
   );
